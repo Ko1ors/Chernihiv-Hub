fafbefad999fd8974b4550d60304e627638b87b3
vladi <vladislavvasilenko2001@gmail.com>
2021-03-19 23:49:28 +0300

    weather start update

---
+++ Chern-App/Clouds.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chern_App
+{
+    public class Clouds
+    {
+        public float All { get; set; }
+    }
+}
---
+++ Chern-App/WindInfo.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chern_App
+{
+    public class WindInfo
+    {
+        public float Speed { get; set; }
+    }
+}
