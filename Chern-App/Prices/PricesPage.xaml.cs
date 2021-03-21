f8b3589ee1d34dc0afe1cdb3c8f49bfb89376e67
Vlad <2011maxwell2011@ukr.net>
2021-03-21 11:09:59 +0200

    start commit

---
+++ Chern-App/Prices/PricesPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using Chern_App.Prices.Models;
+using Chern_App.Prices.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chern_App.Prices
+{
+    /// <summary>
+    /// Логика взаимодействия для PricesPage.xaml
+    /// </summary>
+    public partial class PricesPage : Page
+    {
+        private IViewModel viewModel;
+        private List<Product> products;
+        public PricesPage()
+        {
+            System.Threading.Thread.CurrentThread.CurrentUICulture = System.Globalization.CultureInfo.GetCultureInfo("uk");
+            InitializeComponent();
+            viewModel = new ViewVodelSQLite();
+        }
+
+        private void ButtonShowAll_Click(object sender, RoutedEventArgs e)
+        {
+            products = viewModel.getAllProducts();
+            if (products != null) productListView.ItemsSource = products;
+        }
+
+        private void ButtonSearch_Click(object sender, RoutedEventArgs e)
+        {
+            if (searchProductName.Text.Trim().Length == 0)
+            {
+                MessageBox.Show(Prices.Properties.Resources.ErrorEmptyFieldMessage, Prices.Properties.Resources.ErrorMessageHeader, MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            products = viewModel.getSearchedProducts(searchProductName.Text);
+            if (products != null) productListView.ItemsSource = products;
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo("C://Program Files (x86)//Google//Chrome//Application//chrome.exe", e.Uri.AbsoluteUri));
+            }
+            catch
+            {
+                Process.Start(new ProcessStartInfo("C://Program Files//Google//Chrome//Application//chrome.exe", e.Uri.AbsoluteUri));
+            }
+            finally
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
---
+++ Chern-App/Prices/ViewModels/IViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Chern_App.Prices.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chern_App.Prices.ViewModels
+{
+    interface IViewModel
+    {
+        List<Product> getAllProducts();
+        List<Product> getSearchedProducts(string searchedProductText);
+    }
+}
